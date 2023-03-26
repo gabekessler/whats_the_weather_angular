8862f0950959bea527d091528942d33abf2ab462
Gabe Kessler <gabe@gabesstudio.com>
2023-03-26 15:29:10 -0400

    added forecast and weather components

---
+++ src/app/forecast/forecast.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'forecast',
+  templateUrl: './forecast.component.html',
+  styleUrls: ['./forecast.component.scss']
+})
+export class ForecastComponent {
+
+}
